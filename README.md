154f650f754896c8cbe5ee08c0784f7865b23e8f
ferris <ferris@ferrisellis.com>
2016-02-21 15:14:59 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# lmdbrs_segfault
+# lmdbrs segfault
 Example project that uses lmdb and reproduces mysterious segfault when run.
+
+I've encountered a rather bizarre segfault occurrence that is somehow linked to [lmdb-rs](https://github.com/vhbit/lmdb-rs) by [vhbit](https://github.com/vhbit). The segfault is odd and manifests in strangle places. In my project that I currently am experiencing issues with this it generally manifests when trying to transform data read from the db or move an `Arc` to the environment into a new thread. You can [check out the code here](https://github.com/code-ape/ComposeDB/tree/issue_06).
+
+I created this repository purely for reference in helping solve this issue, which I have filed an issue for [here](https://github.com/vhbit/lmdb-rs/issues/29).
--- src/api/core.rs
+++ src/api/core.rs
@@ -2,8 +2,11 @@ use env_logger;
 use std::path::Path;
 use lmdb::{EnvBuilder, DbFlags};
 
+// general action that works exactly
+// as expected, no issues
 pub fn action1() {
     env_logger::init().unwrap();
+    debug!("checkpoint 1.A");
     let num : u8 = {
         match get_vec() {
             Ok(val) => {
@@ -15,6 +18,7 @@ pub fn action1() {
             }
         }
     };
+    debug!("checkpoint 1.B");
     debug!("num = {}", num);
 
 }
@@ -27,6 +31,7 @@ pub fn action2() {
     let path = Path::new("lmdb_data");
     let env = Box::new(EnvBuilder::new().open(path, 0o777).unwrap());
 
+    debug!("checkpoint 2.A");
     let num : u8 = {
         let db_handle = env.get_default_db(DbFlags::empty()).unwrap();
         let reader = env.get_reader().unwrap();
@@ -41,5 +46,6 @@ pub fn action2() {
             }
         }
     };
-    debug!("num = {}", num);
+    debug!("checkpoint 2.B");
+    println!("num = {}", num);
 }
